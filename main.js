66b701d9c1c436f27ab480ca40a5601730d4b877
guga-ggw <ggwomega15@gmail.com>
2024-05-10 17:43:28 +0400

    fet

--- main.js
+++ main.js
@@ -1,93 +1,84 @@
 
 const icon = document.getElementById("search");
 const search_input = document.getElementById('search-input');
-let CarMan
-let carData
+const selector_manid = document.getElementById('select_manid')
+const select_model = document.getElementsByClassName('select_model')
+const select_type = document.getElementById('select_type')
+const submit_button = document.getElementById('submit_button')
+let manidopt = true
+let carModelList
+let AllData
 
+
+
+
+selector_manid.addEventListener('change', () => {
+    if (typeof (search_input.value == 'number')) {
+        fetchManId()
+        select_model[0].classList.add('enabled_select')
+    }
+
+})
+
+submit_button.addEventListener('click', () => {
+    submitfetch()
+})
 
 icon.addEventListener('click', () => {
     search_input.classList[0] === "opened" ? (search_input.classList.remove('opened'), search_input.classList.add('closed')) : (search_input.classList.add('opened'), search_input.classList.remove('closed'));
 });
-const debounce = (func, delay) => {
-    let timeoutId;
-    return (...args) => {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            func.apply(null, args);
-        }, delay);
-    };
+
+const fetchBrendList = async () => {
+    const res = await fetch('https://static.my.ge/myauto/js/mans.json')
+    const data = await res.json()
+    carModelList = data
+    setselector()
+}
+
+const fetchManId = async () => {
+    const res = await fetch(`https://api2.myauto.ge/ka/getManModels?man_id=` + selector_manid.value);
+    let data = await res.json();
+    select_model[0].innerHTML = '';
+    AllData = data.data
+
+    data.data.forEach((item) => {
+        let optionBrend = item.model_name;
+        const option = document.createElement('option');
+        option.value = item.model_id;
+        option.textContent = optionBrend;
+        select_model[0].appendChild(option);
+    });
 };
 
-search_input.addEventListener('input', debounce(async () => {
-    await fetchData(search_input.value);
-}, 2000));
+const submitfetch = async () => {
+    console.log(AllData)
+    console.log(selector_manid.value, select_model[0].value, select_type.value)
+    const apiUrl = `https://api2.myauto.ge/ka/products`;
 
-const fetchData = async (params) => {
-
-    if (params) {
-        const man_id = CarMan?.find((item) => (
-            item.man_name == params.toUpperCase()
-        )).man_id
-
-        try {
-            if (man_id) {
-                const response = await fetch(`https://api2.myauto.ge/ka/products`, {
-                    params: {
-                        Mans: man_id
-                    }
-                })
-                const data = await response.json();
-                console.log(data)
-                carData = data;
-            } else {
-                // const response = await fetch(`https://api2.myauto.ge/ka/products`)
-                // const data = await response.json();
-                // console.log(data)
-                // carData = data;
-            }
-
-
-
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-        }
-        renderData();
-
-    }
-    else {
-        try {
-            const response = await fetch(`https://static.my.ge/myauto/js/mans.json`);
-            if (!response.ok) {
-                throw new Error('Failed to fetch data');
-            }
-            const data = await response.json();
-            CarMan = data;
-            carData = data
-            console.log(data)
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-        }
-
-
-
-        renderData();
-
-    }
-
-
-};
-
-const renderData = () => {
-    if (carData) {
-        console.log('Rendering data');
-        carData.data?.items.forEach(item => {
-            const div = document.createElement('div');
-            const img = document.createElement('img');
-            img.src = `https://static.my.ge/myauto/photos/${item.photo}/thumbs/${item.car_id}_1.jpg?v=${item.photo_ver}`
-            div.appendChild(img);
-            document.body.appendChild(div);
+    try {
+        const response = await axios.get('https://api2.myauto.ge/ka/products', {
+            params: {
+                Mans: `${selector_manid.value}${select_model[0].value}`,
+                ForRent: select_type.value == 1 ? "0" : "1",
+            },
         });
-    }
-};
 
-fetchData();
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+const setselector = () => {
+    carModelList?.map((item) => {
+        let optionBrend = item.man_name
+        const option = document.createElement('option')
+        option.value = item.man_id
+        option.textContent = optionBrend
+        selector_manid.appendChild(option)
+    })
+}
+
+
+
+fetchBrendList()
